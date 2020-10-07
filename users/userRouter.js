e822038a65130c8309478c094031c6e2fb122af3
Sean Kramer <seandominickramer@gmail.com>
2020-10-06 20:22:05 -0400

    Add endpoint to delete user

--- users/userRouter.js
+++ users/userRouter.js
@@ -32,7 +32,7 @@ router.get('/users/:id', validateUserId(), (req, res) => {
   res.status(200).json(req.user)
 });
 
-router.get('/users/:id/posts', (req, res) => {
+router.get('/users/:id/posts', validateUserId(), (req, res) => {
   users.getUserPosts(req.params.id)
   .then((posts) => {
     res.status(200).json(posts)
@@ -42,8 +42,23 @@ router.get('/users/:id/posts', (req, res) => {
   })
 });
 
-router.delete('/:id', (req, res) => {
-  // do your magic!
+router.delete('/users/:id', validateUserId(), (req, res) => {
+  users
+  .remove(req.params.id)
+  .then((count) => {
+    if(count > 0) {
+      res.status(200).json({
+        message: "User has been deleted",
+      })
+    } else {
+      res.status(404).json({
+        message: "User not found",
+      })
+    }
+  })
+  .catch((error) => {
+    next(error)
+  })
 });
 
 router.put('/:id', (req, res) => {
